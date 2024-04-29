cfd4314a054ec1a9b3f702096da36a4dea1e344c
Kishan <payadikishan@gmail.com>
2024-04-29 12:14:01 +0530

    TLE.

---
+++ CodeforcesTLE/M.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void solve(){
+    // Three cases to solve
+    // First is the case where even, then return the divided by 2.
+    // Second case is when it is odd.
+        // Then iterate from i = 1 to =n/2
+        // For each of the iteration find if its substracted portion if both of them are 
+}
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    solve();
+	   // cout<<endl;
+	}
+	return 0;
+}
